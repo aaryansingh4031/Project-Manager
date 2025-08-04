36f1dd1a44760ee8ef74001659782c4fb37f3821
Aaryan Singh <aaryansingh4031@gmail.com>
2025-08-04 23:09:01 +0530

    Workspace Details

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 
 import authRoutes from "./auth.js";
+import workspaceRoutes from "./workspace.js";
 import userRoutes from "./user.js";
 
-
-const router  = express.Router()
+const router = express.Router();
 
 router.use("/auth", authRoutes);
+router.use("/workspaces", workspaceRoutes);
+router.use("/users", userRoutes);
 
 export default router;
--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -11,5 +11,11 @@ export default [
     ]),
     layout("routes/routes/dashboard/dashboard-layout.tsx", [
         route("dashboard", "routes/routes/dashboard/index.tsx"),
+        route("workspaces", "routes/dashboard/workspaces/index.tsx"),
+        route(
+                "workspaces/:workspaceId",
+                "routes/dashboard/workspaces/workspace-details.tsx"
+            ),
+        
     ]),
 ] satisfies RouteConfig;
