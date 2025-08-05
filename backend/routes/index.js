45769bf0b4de83ad26ece52a1b64c6bfb969f38a
Aaryan Singh <aaryansingh4031@gmail.com>
2025-08-05 23:28:30 +0530

    Create Project Page

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,12 +2,14 @@ import express from "express";
 
 import authRoutes from "./auth.js";
 import workspaceRoutes from "./workspace.js";
+import projectRoutes from "./project.js";
 import userRoutes from "./user.js";
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/workspaces", workspaceRoutes);
+router.use("/projects", projectRoutes);
 router.use("/users", userRoutes);
 
 export default router;
